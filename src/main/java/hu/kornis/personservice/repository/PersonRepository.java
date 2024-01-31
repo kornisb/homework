79e15928f87fe66b630a8290aeb59dcedd4b8a25
Bence Kornis <bence.kornis@gmail.com>
2024-01-31 23:57:48 +0100

    Added real implementation to Service + Repository moved to proper package

--- src/main/java/hu/kornis/personservice/repository/PersonRepository.java
+++ src/main/java/hu/kornis/personservice/repository/PersonRepository.java
@@ -1,6 +1,8 @@
-package hu.kornis.personservice.model;
+package hu.kornis.personservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import hu.kornis.personservice.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
 	
--- src/main/java/hu/kornis/personservice/service/PersonService.java
+++ src/main/java/hu/kornis/personservice/service/PersonService.java
@@ -3,11 +3,13 @@ package hu.kornis.personservice.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import hu.kornis.personservice.model.Person;
-import hu.kornis.personservice.model.PersonRepository;
+import hu.kornis.personservice.repository.PersonRepository;
 
 @Service
 public class PersonService {
@@ -16,22 +18,42 @@ public class PersonService {
 	private PersonRepository personRepository;
 
 	public ResponseEntity<List<Person>> getAllPersons() {
-		return ResponseEntity.ok().build();
+		return ResponseEntity.ok().body(personRepository.findAll());
 	}
 
 	public ResponseEntity<Person> getPersonById(int id) {
-		return ResponseEntity.ok().build();
+		return personRepository.findById(id)
+	            .map(ResponseEntity::ok)
+	            .orElseGet(() -> ResponseEntity.notFound().build());
 	}
 
 	public ResponseEntity<Person> addPerson(Person person) {
-		return ResponseEntity.ok().build();
+		return new ResponseEntity<>(personRepository.save(person), HttpStatus.CREATED);
 	}
 
+	@Transactional
 	public ResponseEntity<Person> updatePersonById(int id, Person person) {
-	    return ResponseEntity.ok().build();
+	    return personRepository.findById(id)
+	            .map(existingPerson -> {
+	                existingPerson.setFirstName(person.getFirstName());
+	                existingPerson.setLastName(person.getLastName());
+	                existingPerson.setAddresses(person.getAddresses());
+	                existingPerson.setContacts(person.getContacts());
+	                return existingPerson;
+	            })
+	            .map(personRepository::save)
+	            .map(ResponseEntity::ok)
+	            .orElseGet(() -> ResponseEntity.notFound().build());
 	}
 	
 	public ResponseEntity<String> deletePersonById(int id) {
-		return ResponseEntity.ok().build();
+		ResponseEntity<String> response;
+		if (personRepository.existsById(id)) {
+			personRepository.deleteById(id);
+			response = ResponseEntity.noContent().build();
+		} else {
+			response = ResponseEntity.notFound().build();
+		}
+		return response;
 	}
 }
